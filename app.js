bd3e01525cb0a25a73858464626fda6aac136856
Shahjahan Akbar <125791338+shahjahanakbar@users.noreply.github.com>
2024-04-03 04:14:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function(){
+     let h2 = document.querySelector("h2");
+     let randomcolor = getRandomColor();
+     h2.innerText = randomcolor;
+     
+     let div = document.querySelector("div");
+     div.style.backgroundColor = randomcolor;
+     console.log("Color Updated");
+});
+
+function getRandomColor(){
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let color = `rgb(${red},${green},${blue})`;
+    return color;
+}
